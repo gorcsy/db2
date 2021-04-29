a8bcbbc7ecae6bfe180c52d51838f8b58cd90c22
Görcs Balázs <gorcs.balazs@gmail.com>
2021-04-29 11:21:49 +0200

    green tests

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -10,6 +14,21 @@ public class JurassicPark {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+
+    public List<String> checkOverpopulation() {
+        List<String> result = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String sql = "SELECT * FROM dinosaur WHERE actual > expected ORDER BY breed";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                result.add(resultSet.getString("breed"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("SQL exception occurred");
+        }
+        return result;
     }
 
 }
